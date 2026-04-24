8c7496af7c5e83fb732f0e9d29e8544e56e6877d
butaz07 <elianchavezfranco@gmail.com>
2026-04-24 02:03:12 -0400

    Update TeamDTOs.cs

--- RoboManager/RoboManager.Application/DTOs/TeamDTOs.cs
+++ RoboManager/RoboManager.Application/DTOs/TeamDTOs.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoboManager.Application.DTOs
+﻿namespace RoboManager.Application.DTOs
 {
-    internal class TeamDTOs
+    // DTO para mostrar la información (Lectura)
+    public class TeamDto
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+    }
+
+    // DTO para recibir datos del Frontend (Creación/Actualización)
+    public class TeamCreateDto
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
     }
 }
